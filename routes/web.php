efa7f10fab399c471c8c10ae467d92991ec499f3
hrtsnam <hrtsnam@gmail.com>
2026-05-07 17:29:03 +0700

    added landing page v1

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,158 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $products = [
+    [
+        "link" => "#",
+        "discount" => 20,
+        "image" => "assets/img/products/pinkjersey.png",
+        "name" => "Jersey RPL Pink",
+        "price" => "Rp120.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 10,
+        "image" => "assets/img/products/workjacket.png",
+        "name" => "Work Jakcet RPL",
+        "price" => "Rp207.000",
+        "rating" => 4.9,
+        "reviews" => 49,
+    ],
+    [
+        "link" => "#",
+        "discount" => 30,
+        "image" => "assets/img/products/skyjersey.png",
+        "name" => "Jersey RPL Sky Blue",
+        "price" => "Rp105.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 0,
+        "image" => "assets/img/products/stiker1.png",
+        "name" => "Stiker Minnix Series",
+        "price" => "Rp10.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 0,
+        "image" => "assets/img/products/stiker2.png",
+        "name" => "Stiker Devoria Series",
+        "price" => "Rp10.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 20,
+        "image" => "assets/img/products/pinkjersey.png",
+        "name" => "Jersey RPL Pink",
+        "price" => "Rp120.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 10,
+        "image" => "assets/img/products/workjacket.png",
+        "name" => "Work Jakcet RPL",
+        "price" => "Rp207.000",
+        "rating" => 4.9,
+        "reviews" => 49,
+    ],
+    [
+        "link" => "#",
+        "discount" => 30,
+        "image" => "assets/img/products/skyjersey.png",
+        "name" => "Jersey RPL Sky Blue",
+        "price" => "Rp105.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 0,
+        "image" => "assets/img/products/stiker1.png",
+        "name" => "Stiker Minnix Series",
+        "price" => "Rp10.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 0,
+        "image" => "assets/img/products/stiker2.png",
+        "name" => "Stiker Devoria Series",
+        "price" => "Rp10.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+            "link" => "#",
+        "discount" => 20,
+        "image" => "assets/img/products/pinkjersey.png",
+        "name" => "Jersey RPL Pink",
+        "price" => "Rp120.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 10,
+        "image" => "assets/img/products/workjacket.png",
+        "name" => "Work Jakcet RPL",
+        "price" => "Rp207.000",
+        "rating" => 4.9,
+        "reviews" => 49,
+    ],
+    [
+        "link" => "#",
+        "discount" => 30,
+        "image" => "assets/img/products/skyjersey.png",
+        "name" => "Jersey RPL Sky Blue",
+        "price" => "Rp105.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 0,
+        "image" => "assets/img/products/stiker2.png",
+        "name" => "Stiker Devoria Series",
+        "price" => "Rp10.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 0,
+        "image" => "assets/img/products/stiker1.png",
+        "name" => "Stiker Minnix Series",
+        "price" => "Rp10.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    [
+        "link" => "#",
+        "discount" => 20,
+        "image" => "assets/img/products/pinkjersey.png",
+        "name" => "Jersey RPL Pink",
+        "price" => "Rp120.000",
+        "rating" => 4.7,
+        "reviews" => 47,
+    ],
+    ];
+    return view('index', compact('products'));
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
 
 Route::get('/admin', function () {
     return view('admin/dashboard');
